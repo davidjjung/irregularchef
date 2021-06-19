73cf4a6ca062b6e22d5bde96e9e79429fca3fcd6
davidjjung <david.j.jung@gmail.com>
2021-06-19 13:41:17 -0400

    mostly textures

--- src/main/java/com/davigj/irregularchef/core/registry/IrregularChefItems.java
+++ src/main/java/com/davigj/irregularchef/core/registry/IrregularChefItems.java
@@ -78,7 +78,7 @@ public class IrregularChefItems {
 		public static final Food POOL_PARTY_STICK = (new Food.Builder()).hunger(9).saturation(0.6F).build();
 		public static final Food PURPLE_COW_FLOAT = (new Food.Builder()).hunger(4).saturation(0.6F).setAlwaysEdible().effect(() -> new EffectInstance(Effects.LEVITATION, 120), 1.0F).build();
 		public static final Food TURTLE_GALLIMAUFRY = (new Food.Builder()).hunger(13).saturation(0.8F).effect(() -> new EffectInstance(Effects.WATER_BREATHING, 1800), 1.0F).effect(() -> new EffectInstance(ModEffects.NOURISHED.get(), 6000), 1.0F).build();
-		public static final Food HUNTERS_CASSEROLE = (new Food.Builder()).hunger(11).saturation(0.7F).effect(() -> new EffectInstance(Effects.INVISIBILITY, 300, 0, true, false), 1.0F).effect(() -> new EffectInstance(Effects.STRENGTH, 200, 1, false, true), 1.0F).build();
+		public static final Food HUNTERS_CASSEROLE = (new Food.Builder()).hunger(8).saturation(0.6F).effect(() -> new EffectInstance(Effects.INVISIBILITY, 300, 0, true, false), 1.0F).build();
 		public static final Food CHARCOAL_WAFFLES = (new Food.Builder()).hunger(4).saturation(0.8F).effect(() -> new EffectInstance(Effects.WEAKNESS, 600, 0, true, true), 0.7F).effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 600, 0, false, true), 1.0F).build();
 	}
 }
